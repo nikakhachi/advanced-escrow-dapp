37aa85531f5d825ec9cbbd432a17a23fb36b6804
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2023-04-29 13:09:47 +0400

    add contracts tests for owner actions

--- test/Escrow.ts
+++ test/Escrow.ts
@@ -17,7 +17,6 @@ describe("Escrow", function () {
   describe("Deployment", function () {
     it("Should set the right owner", async function () {
       const { contract, owner } = await loadFixture(deployEscrowFixture);
-
       expect(await contract.owner()).to.equal(owner.address);
     });
     it("Should set the right admin", async function () {
@@ -39,6 +38,49 @@ describe("Escrow", function () {
     it("Initial escrows length should be zero", async function () {
       const { contract } = await loadFixture(deployEscrowFixture);
       expect((await contract.getAllEscrows()).length).to.equal(0);
+    });
+  });
+
+  describe("Owner Actions", function () {
+    it("Should set agent fee percentage correctly", async function () {
+      const { contract } = await loadFixture(deployEscrowFixture);
+      const UPDATE_AGENT_FEE_PERCENTAGE = 25;
+      await contract.changeAgentFeePercentage(UPDATE_AGENT_FEE_PERCENTAGE);
+      expect(await contract.agentFeePercentage()).to.equal(UPDATE_AGENT_FEE_PERCENTAGE);
+    });
+    it("Should withdraw funds correctly", async function () {
+      const { contract, owner } = await loadFixture(deployEscrowFixture);
+      expect(await contract.owner()).to.equal(owner.address);
+    });
+    it("Should add an agent", async function () {
+      const { contract, acc2 } = await loadFixture(deployEscrowFixture);
+      expect(await contract.hasRole(ethers.utils.id("AGENT_ROLE"), acc2.address)).to.be.false;
+      await contract.addAgent(acc2.address);
+      expect(await contract.hasRole(ethers.utils.id("AGENT_ROLE"), acc2.address)).to.be.true;
+    });
+    it("Should revoke an agent", async function () {
+      const { contract, acc3 } = await loadFixture(deployEscrowFixture);
+      expect(await contract.hasRole(ethers.utils.id("AGENT_ROLE"), acc3.address)).to.be.false;
+      await contract.addAgent(acc3.address);
+      expect(await contract.hasRole(ethers.utils.id("AGENT_ROLE"), acc3.address)).to.be.true;
+      await contract.revokeAgent(acc3.address);
+      expect(await contract.hasRole(ethers.utils.id("AGENT_ROLE"), acc3.address)).to.be.false;
+    });
+    it("Should not allow non-owner to set agent fee percentage", async function () {
+      const { contract, acc1 } = await loadFixture(deployEscrowFixture);
+      await expect(contract.connect(acc1).changeAgentFeePercentage(25)).to.revertedWith("Ownable: caller is not the owner");
+    });
+    it("Should not allow non-owner to withdraw funds", async function () {
+      const { contract, acc1 } = await loadFixture(deployEscrowFixture);
+      await expect(contract.connect(acc1).withdrawFunds(5)).to.revertedWith("Ownable: caller is not the owner");
+    });
+    it("Should not allow non-owner to add an agent", async function () {
+      const { contract, acc1, acc2 } = await loadFixture(deployEscrowFixture);
+      await expect(contract.connect(acc1).addAgent(acc2.address)).to.revertedWith("Ownable: caller is not the owner");
+    });
+    it("Should not allow non-owner to revoke an agent", async function () {
+      const { contract, acc1, acc2 } = await loadFixture(deployEscrowFixture);
+      await expect(contract.connect(acc1).revokeAgent(acc2.address)).to.revertedWith("Ownable: caller is not the owner");
     });
   });
 });
