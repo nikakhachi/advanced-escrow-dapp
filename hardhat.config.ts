b8bd7dd9fc866cca5b9ac41bf57b7a3c418d69e5
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2023-04-29 16:34:56 +0400

    add networks to hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,14 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: process.env.ALCHEMY_TESTNET_URL,
+      accounts: [process.env.PRIVATE_KEY as string],
+    },
+  },
 };
-
 export default config;
