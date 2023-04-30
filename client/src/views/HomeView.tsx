60c2cbf5efdc0c160b3fd1c028d67e6fb5402e47
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2023-04-30 12:58:56 +0400

    add tailwind to client

--- client/src/views/HomeView.tsx
+++ client/src/views/HomeView.tsx
@@ -5,5 +5,9 @@ import { EscrowAgentContext } from "../contexts/EscrowAgentContext";
 export const HomeView = () => {
   const escrowAgentContext = useContext(EscrowAgentContext);
 
-  return <Container sx={{ paddingTop: "1rem" }}>Home View</Container>;
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <h1 className="text-3xl font-bold text-blue-600">Install & Setup Vite + React + Typescript + Tailwind CSS 3</h1>
+    </div>
+  );
 };
